074ad917cc41ac128d5b34729e34ef67154c8675
LuiTOR-PC\LuiTOR <luitorc@gmail.com>
2017-07-02 21:23:15 -0500

    Revert "no message"
    This reverts commit 052c4f03f57d88aa0ea08f5dbf66b56f76b55c46.

--- public/js/init.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var url = window.location.href;
-    var value = url.substring(url.lastIndexOf('/') + 1);
-    $("#menu1 a").removeClass('active');
-    $("#menu1 a[href='"+value+"']").addClass('active');		
-});
-// function lt_option_select(){
-	
-// }
