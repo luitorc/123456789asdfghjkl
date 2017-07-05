b8c72c615e0eb90e4bf922daee72933d8352a8aa
luitorc <luitorc@gmail.com>
2017-07-05 00:01:04 -0500

    Update luitor_helper.js

--- helper/luitor_helper.js
+++ helper/luitor_helper.js
@@ -80,15 +80,7 @@ exports.connection = function(type) {
 	switch(type){
 		case 'pg':
 			var pg = require('pg');
-			var config = {
-				host: 'a2ss35.a2hosting.com', // Server hosting the postgres database 
-				user: 'iloredco', //env var: PGUSER 
-				password: 'supermario123', //env var: PGPASSWORD 
-				database: 'iloredco_sistema_optica_mod01', //env var: PGDATABASE 
-				port: 5432, //env var: PGPORT 
-				max: 50, // max number of clients in the pool 
-				idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
-			};
+			
 			// console.log("entro")
 			var pool = new pg.Pool(config);
 			return pool;
