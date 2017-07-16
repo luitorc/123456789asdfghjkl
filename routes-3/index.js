8f6962e28f0919bcd3ffe3de68ebb2cd1fcd97d7
LuiTOR-PC\LuiTOR <luitorc@gmail.com>
2017-07-16 15:49:48 -0500

    no message

---
+++ routes-3/index.js
@@ -0,0 +1,66 @@
+
+exports.admin_publish = function(req, res){
+	var params = {
+		title: "Publicación"
+	};
+	res.render('admin/publish', params);
+}
+exports.admin_patientRecord = function(req, res){
+	var params = {
+		title: "Expediente del Paciente"
+		,idp: req.query.idp!=undefined?req.query.idp:0
+	};
+	res.render('admin/patientRecord', params);
+}
+exports.admin_mypatients = function(req, res){
+	var params = {
+		title: "Nueva Receta"
+	};
+	res.render('admin/mypatients', params);
+}
+exports.admin_newrecipe = function(req, res){
+	var params = {
+		title: "Nueva Receta"
+		,idp: req.query.idp!=undefined?req.query.idp:0
+		,idr: req.query.idr!=undefined?req.query.idr:0
+		,fullname: req.query.fullname!=undefined?req.query.fullname:''
+	};
+	res.render('admin/newrecipe', params);
+}
+
+exports.admin_registerp = function(req, res){
+	// console.log(req.query.edit);
+	var params = {
+		title: "Registro de Paciente"
+		,idp: req.query.idp!=undefined?req.query.idp:0
+		,edit: req.query.edit==""?true:false
+	};
+	res.render('admin/registerp', params);
+}
+
+
+exports.admin = function(req, res){
+	var params = {
+		title: "Admin"
+	};
+	res.render('admin/index', params);
+}
+
+exports.index = function(req, res){
+	var params = {
+		title: "Principal"
+	};
+	res.render('index', params);
+}
+exports.login = function(req, res){
+	var params = {
+		title: "Aces"
+	};
+	res.render('login', params);
+}
+exports.contact = function(req, res){
+	var params = {
+		title: "Contactanos"
+	};
+	res.render('contact', params);
+}
